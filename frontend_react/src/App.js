b7ff4ea2368587cb7d93bb99c9d5fc48efb51204
AaaronPM <aaron.perez989@gmail.com>
2022-08-16 18:30:29 -0700

    Porfolio completed need to change filler pictures

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -15,7 +15,7 @@ const App = () => {
             <Work />
             <Skills />
             <Testimonial />
-            <Work />
+            <Footer/>
         </div>
     )
 }
--- frontend_react/src/index.js
+++ frontend_react/src/index.js
@@ -1,7 +1,13 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 
 import App from './App'
 import './index.css'
 
-ReactDOM.render(<App />,document.getElementById('root'))
+
+//ReactDOM.render(<App />,document.getElementById('root'))
+//DONT FORGET NEW REACT 18 syntax
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <App/>
+)
